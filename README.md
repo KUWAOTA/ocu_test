fb8455156da08672a6891e406d1ac12c04381485
DESKTOP-ACONK0H\user <unosyukatsu@gmail.com>
2021-01-15 15:43:26 +0900

    feat: testproject for script/debug and new project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# ocu_test
-oculus linkを使った開発のテストリポジトリ
+version https://git-lfs.github.com/spec/v1
+oid sha256:7fb97ab589448e989ba242bad22afa001ceca7263ef87d6481a978c33c5eb01a
+size 70
---
+++ test4debug/Assets/Script/test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("経過時間(秒)" + Time.time);
+        if (Time.time > 10)
+        {
+            Debug.Log("anal" + Time.time);
+        }
+    }
+}
